b53cbeb56009b75a1aecf2d2e8f30fbde5df31eb
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2014-06-06 16:26:24 +0800

    complete: login, logout, parsing

--- core.py
+++ core.py
@@ -12,10 +12,82 @@ db_user = {
 }
 
 # Hardcoded input for testing
-input = (
-	"Banana login admin password",
-	"Banana Basecamp Add 06/06/14 2.5 'Daenerys Targaryen' 'feed dragons'",
-	"Banana logout"
+input_commands = (
+    "banana login admin password",
+    "banana basecamp add 06/06/14 2.5 'Daenerys Targaryen' 'feed dragons'",
+    "banana logout",
 )
 
-print input 
+# Hardcoded error message
+error = {
+    'error_unknown' : 'Unknown Command.',
+    'error_syntax' : 'Syntax Error.',
+    'error_already_logged' : 'You are already logged in. Log current account first.',
+    'error_login' : 'User does not exsist or username and password is incorrect.',
+}
+
+# Hardcoded notif message
+notif = {
+    'notif_login_success' : 'Login Success',
+    'notif_logout_success' : 'You have been successfully logged out.',
+}
+
+
+##########################################################
+
+logged_user = None
+
+##########################################################
+
+# Parses and identifies the command
+def parse_command(input):
+    tokens = input.split(' ')
+
+    if tokens[0] == "banana":
+        
+        if tokens[1] == "login":
+            login(tokens)
+        
+        if tokens[1] == "logout":
+            logout()
+
+        if tokens[1] == "basecamp":
+            pass
+
+
+#
+def logout():
+
+    logged_user = None
+    print notif['notif_logout_success']
+
+
+# checks credentials and login user
+def login(tokens):
+
+    global logged_user
+
+    correct_credentials = False
+
+    for user in db_user.items():
+        if user[1]['username'] == tokens[2] and user[1]['password'] == tokens[3]:
+
+            correct_credentials = True
+            
+            if  logged_user is not None:
+                print error['error_already_logged']
+
+            elif user[1]['username'] == logged_user:
+                print error['error_already_logged']
+
+            else:
+                logged_user = user[1]['username']
+                print notif['notif_login_success']
+
+    if not correct_credentials:
+        print error['error_login']
+
+
+##########################################################
+
+parse_command(input_commands[0])
