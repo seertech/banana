9c9fc9fb4a6b2a7fc1045be1bc9c0516a6e69490
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2014-06-06 15:02:16 +0800

    initial hardcoded data

---
+++ core.py
@@ -0,0 +1,21 @@
+# Hardcoded user credentials for logging in
+db_user = {
+	1 : {
+		'username' : 'admin',
+		'password' : 'password'
+	},
+
+	2 : {
+		'username' : 'foo',
+		'password' : 'bar'
+	}
+}
+
+# Hardcoded input for testing
+input = (
+	"Banana login admin password",
+	"Banana Basecamp Add 06/06/14 2.5 'Daenerys Targaryen' 'feed dragons'",
+	"Banana logout"
+)
+
+print input 
